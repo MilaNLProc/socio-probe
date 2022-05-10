47e2322f76ab687a6955a8142f0d7e0093e79916
vinid <chiccobia@gmail.com>
2022-05-10 21:27:08 +0200

    change validation

--- custom_dataset.py
+++ custom_dataset.py
@@ -27,7 +27,7 @@ def tt(tokenizer):
 
         return tokenizer(
             examples["texts"],
-            padding=True, truncation=True, max_length=True)
+            padding=True, max_length=300)
     return tokenize_function
 
 def prepare_dataset(dataset, tokenizer):
