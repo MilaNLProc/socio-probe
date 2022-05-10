3f7fb7258307073dd85c7ac928f6110951973f2f
vinid <chiccobia@gmail.com>
2022-05-10 19:22:58 +0200

    micro change

--- probers.py
+++ probers.py
@@ -251,7 +251,7 @@ class Embedder:
 
     def mean_pooling(self, token_embeddings, attention_mask):
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9).detach().numpy()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9).detach().cpu().numpy()
 
     def create_embeddings(self, texts, labels, layers: List, embedding_path):
 
