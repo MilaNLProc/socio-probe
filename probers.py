428300a39c0ec7666726e61363ebd1f87c4264a0
vinid <chiccobia@gmail.com>
2022-06-07 13:34:18 +0200

    changing the entire structure of the training

--- probers.py
+++ probers.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -11,12 +12,12 @@ from torch import nn
 import random
 import string
 
-
 from sklearn.metrics import f1_score
 from torch.utils.data import DataLoader
 
 from custom_dataset import *
 import pickle
+
 
 class MLP(nn.Module):
     """
@@ -36,10 +37,92 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
+
+class MLPTrainer:
+
+    def __init__(self, embedding_size, output_size, hiddens, device):
+        self.device = device
+        self.embedding_size = embedding_size
+        self.output_size = output_size
+        self.hiddens = hiddens
+
+    def train(self, trainloader, validloader, testloader, name, epochs=100, patience=5):
+
+        self.mlp = MLP(self.embedding_size, self.output_size, self.hiddens)
+        self.mlp.to(self.device)
+
+        loss_function = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.mlp.parameters(), lr=1e-3)
+        scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5)
+
+        early_stopping = EarlyStopping(patience=patience, verbose=False, path=name)
+
+        for epoch in range(0, epochs):
+
+            for i, data in enumerate(trainloader, 0):
+                self.mlp.train()
+
+                inputs, targets = data
+
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
+
+                optimizer.zero_grad()
+                outputs = self.mlp(inputs)
+
+                loss = loss_function(outputs, targets)
+                loss.backward()
+
+                optimizer.step()
+
+            valid_loss = 0
+            self.mlp.eval()
+            with torch.no_grad():
+
+                for i, data in enumerate(validloader, 0):
+                    inputs, targets = data
+                    inputs = inputs.to(self.device)
+                    targets = targets.to(self.device)
+
+                    optimizer.zero_grad()
+                    outputs = self.mlp(inputs)
+
+                    valid_loss += loss_function(outputs, targets)
+
+            scheduler.step(valid_loss)
+            early_stopping(valid_loss, self.mlp)
+
+            if early_stopping.early_stop:
+                break
+
+        mlp = MLP(self.embedding_size, self.output_size, self.hiddens)
+        mlp.load_state_dict(torch.load(name))
+        mlp.to(self.device)
+        mlp.eval()
+
+        predictions = []
+        test_loss = 0
+        with torch.no_grad():
+            labels = []
+            for i, data in enumerate(testloader, 0):
+                inputs, targets = data
+
+                inputs = inputs.to(self.device)
+                outputs = mlp(inputs)
+                test_loss += loss_function(outputs, targets).item()
+                predictions.extend(np.argmax(outputs.detach().cpu().numpy(), axis=1).tolist())
+                labels.extend(targets.numpy().tolist())
+
+        os.remove(name)
+
+        return {"f1": f1_score(labels, predictions, average="macro"), "loss": test_loss}
+
+
 class ClassicalProber:
     """
     Prober based on the classical framework
     """
+
     def __init__(self, embedding_size, device="cuda"):
         self.embedding_size = embedding_size
         self.device = device
@@ -57,11 +140,10 @@ class ClassicalProber:
         results = {}
         for l in layers:
             train_X, evaluation_X, train_y, evaluation_y = train_test_split(data[l], labels,
-                                                                            test_size = 0.2,
-                                                                            random_state = 42)
+                                                                            test_size=0.2,
+                                                                            random_state=42)
             eval_X, test_X, eval_y, test_y = train_test_split(evaluation_X, evaluation_y,
-                                                              test_size = 0.5, random_state = 42)
-
+                                                              test_size=0.5, random_state=42)
             results[l] = self.train_and_test(train_X,
                                              train_y,
                                              test_X,
@@ -71,7 +153,6 @@ class ClassicalProber:
                                              hiddens=50, batch_size=batch_size)
 
         return results
-
 
     def train_and_test(self, train_X, train_y, test_X, test_y, eval_X, eval_Y, output_size, hiddens=100, epochs=200,
                        patience=2, batch_size=32):
@@ -85,88 +166,20 @@ class ClassicalProber:
         test_dataset = ProbingDataset(test_X, test_y)
         testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-        mlp = MLP(self.embedding_size, output_size, hiddens)
-        mlp.to(self.device)
-
-        loss_function = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(mlp.parameters(), lr=5e-5)
-
-        validation_steps = int(len(trainloader)/4)
-
         N = 10
         name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N)) + ".pt"
-        early_stopping = EarlyStopping(patience=patience, verbose=False, path=name)
+        mlp = MLPTrainer(self.embedding_size, output_size, hiddens, self.device)
 
-        for epoch in range(0, epochs):
+        return mlp.train(trainloader, validloader, testloader, name, epochs=100, patience=5)
 
-            if early_stopping.early_stop:
-                print("Second Early STOP")
-                break
 
-            for i, data in enumerate(trainloader, 0):
-
-                mlp.train()
-
-                inputs, targets = data
-
-                inputs = inputs.to(self.device)
-                targets = targets.to(self.device)
-
-                optimizer.zero_grad()
-                outputs = mlp(inputs)
-
-                loss = loss_function(outputs, targets)
-                loss.backward()
-
-                optimizer.step()
-
-                if i % validation_steps == 0:
-                    valid_loss = 0
-                    mlp.eval()
-                    with torch.no_grad():
-
-                        for i, data in enumerate(validloader, 0):
-                            inputs, targets = data
-                            inputs = inputs.to(self.device)
-                            targets = targets.to(self.device)
-
-                            optimizer.zero_grad()
-                            outputs = mlp(inputs)
-
-                            valid_loss += loss_function(outputs, targets)
-
-                    early_stopping(valid_loss, mlp)
-
-                if early_stopping.early_stop:
-                    break
-
-            #pbar.update(1)
-
-        mlp = MLP(self.embedding_size, output_size, hiddens)
-        mlp.load_state_dict(torch.load(name))
-        mlp.to(self.device)
-        mlp.eval()
-
-        predictions = []
-
-        with torch.no_grad():
-            labels = []
-            for i, data in enumerate(testloader, 0):
-                inputs, targets = data
-
-                inputs = inputs.to(self.device)
-                outputs = mlp(inputs)
-
-                predictions.extend(np.argmax(outputs.detach().cpu().numpy(), axis=1).tolist())
-                labels.extend(targets.numpy().tolist())
-        os.remove(name)
-        return f1_score(labels, predictions, average="macro")
 
 
 class MLDProber:
     """
     Prober based on MLD
     """
+
     def __init__(self, embedder, embedding_size):
         """
         :param embedder: SentenceTransformer embedding model
@@ -231,40 +244,44 @@ class MLDProber:
             results[l] = {"code_length": code_length_first_portion, "sum_of_losses": sum_of_losses}
         return results
 
-    def get_loss(self, train_X, train_y, test_X, test_y, output_size, hiddens=50, epochs=200):
-        """
-        Simply training
-        :param train_X:
-        :param train_y:
-        :param test_X:
-        :param test_y:
-        :param output_size:
-        :param hiddens:
-        :param epochs:
-        :return:
-        """
-        embedding_train = self.embedder.encode(train_X)
-        embedding_test = self.embedder.encode(test_X)
+    def train_and_test(self, train_X, train_y, test_X, test_y, eval_X, eval_Y, output_size, hiddens=100, epochs=200,
+                       patience=2, batch_size=32):
 
-        train_dataset = ProbingDataset(embedding_train, train_y)
-        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=4)
+        train_dataset = ProbingDataset(train_X, train_y)
+        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-        test_dataset = ProbingDataset(embedding_test, test_y)
-        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=4)
+        valid_dataset = ProbingDataset(eval_X, eval_Y)
+        validloader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+
+        test_dataset = ProbingDataset(test_X, test_y)
+        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
         mlp = MLP(self.embedding_size, output_size, hiddens)
-
-        mlp = mlp.to("cuda")
+        mlp.to(self.device)
 
         loss_function = nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(mlp.parameters(), lr=5e-5)
 
+        validation_steps = int(len(trainloader) / 4)
+
+        N = 10
+        name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N)) + ".pt"
+        early_stopping = EarlyStopping(patience=patience, verbose=False, path=name)
+
         for epoch in range(0, epochs):
+
+            if early_stopping.early_stop:
+                print("Second Early STOP")
+                break
+
             for i, data in enumerate(trainloader, 0):
+
+                mlp.train()
+
                 inputs, targets = data
 
-                inputs = inputs.to("cuda")
-                targets = targets.to("cuda")
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
 
                 optimizer.zero_grad()
                 outputs = mlp(inputs)
@@ -274,20 +291,44 @@ class MLDProber:
 
                 optimizer.step()
 
-        final_loss = 0.0
+                if i % validation_steps == 0:
+                    valid_loss = 0
+                    mlp.eval()
+                    with torch.no_grad():
 
+                        for i, data in enumerate(validloader, 0):
+                            inputs, targets = data
+                            inputs = inputs.to(self.device)
+                            targets = targets.to(self.device)
+
+                            optimizer.zero_grad()
+                            outputs = mlp(inputs)
+
+                            valid_loss += loss_function(outputs, targets)
+
+                    early_stopping(valid_loss, mlp)
+
+                if early_stopping.early_stop:
+                    break
+
+            # pbar.update(1)
+
+        mlp = MLP(self.embedding_size, output_size, hiddens)
+        mlp.load_state_dict(torch.load(name))
+        mlp.to(self.device)
+        mlp.eval()
+
+        predictions = []
+        test_loss = 0
         with torch.no_grad():
+            labels = []
             for i, data in enumerate(testloader, 0):
                 inputs, targets = data
-                optimizer.zero_grad()
+
+                inputs = inputs.to(self.device)
                 outputs = mlp(inputs)
 
-                loss = loss_function(outputs, targets)
-
-                final_loss += loss.item()
-
-        return final_loss
-
-
-
-
+                test_loss += loss_function(outputs, targets)
+                labels.extend(targets.numpy().tolist())
+        os.remove(name)
+        return test_loss
